6d021c232af9c7a383ebca8fa4b69122604884aa
Weblate <noreply@weblate.org>
2023-05-26 16:47:07 +0000

    Added translation using Weblate (Chinese (Simplified))
    Co-authored-by: skywalker512 <houzhenhong@outlook.com>

---
+++ lang/zh_Hans/passwords.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    // https://github.com/laravel/framework/blob/master/src/Illuminate/Translation/lang/en/passwords.php
+    'reset' => '',
+    'sent' => '',
+    'throttled' => '',
+    'token' => '',
+    'user' => "",
+];
