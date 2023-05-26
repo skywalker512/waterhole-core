5a81ffdb1f480b92468a64c4d09f09c6a5a9a7c9
Weblate <noreply@weblate.org>
2023-05-26 16:48:20 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (5 of 5 strings)
    
    Translated using Weblate (Chinese (Simplified))
    
    Currently translated at 18.9% (7 of 37 strings)
    
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: skywalker512 <houzhenhong@outlook.com>
    Translate-URL: http://9.134.54.78:8090/projects/waterhole/auth/zh_Hans/
    Translate-URL: http://9.134.54.78:8090/projects/waterhole/passwords/zh_Hans/
    Translation: waterhole/auth
    Translation: waterhole/passwords

---
+++ lang/zh_Hans/auth.ftl
@@ -0,0 +1,7 @@
+password = 提供的密码不正确。
+login-title = 登录您的账户
+login-submit = 登录
+login-register-prompt = 没有账户？
+login-register-link = 注册
+failed = 这些凭据与我们的记录不符。
+throttle = 登录尝试过多。请在 { $seconds } 秒后重试。
--- lang/zh_Hans/passwords.php
+++ lang/zh_Hans/passwords.php
@@ -1,9 +1,9 @@
 <?php
 return [
     // https://github.com/laravel/framework/blob/master/src/Illuminate/Translation/lang/en/passwords.php
-    'reset' => '',
-    'sent' => '',
-    'throttled' => '',
-    'token' => '',
-    'user' => "",
+    'reset' => '您的密码已重置！',
+    'sent' => '我们已经通过电子邮件发送了您的密码重置链接！',
+    'throttled' => '请在重试之前等待。',
+    'token' => '此密码重置令牌无效。',
+    'user' => "我们找不到使用该电子邮件地址的用户。",
 ];
